53bca31da4cc6838683ce379036e03f8ae9d1d62
ollycassidy13 <ollyj.cassidy@gmail.com>
2024-08-04 13:43:02 +0100

    logging updates

--- nids/logging.py
+++ nids/logging.py
@@ -1,19 +1,20 @@
 # nids/logging.py
 
 import logging
+from logging.handlers import RotatingFileHandler
 from scapy.all import IP, IPv6
 import os
 
 def setup_logging():
-    # Clear the log file if it exists
     log_file = 'nids_logs.log'
-    if os.path.exists(log_file):
-        with open(log_file, 'w'):
-            pass
-    
+    max_log_size = 5 * 1024 * 1024  # 5 MB
+    backup_count = 3
+
     # Setup logging configuration
-    logging.basicConfig(filename=log_file, level=logging.INFO,
-                        format='%(asctime)s:%(levelname)s:%(message)s')
+    handler = RotatingFileHandler(log_file, maxBytes=max_log_size, backupCount=backup_count)
+    logging.basicConfig(level=logging.INFO,
+                        format='%(asctime)s:%(levelname)s:%(message)s',
+                        handlers=[handler])
 
 def log_prediction(packet, prediction, original_data, traffic_type, src_ip, dst_ip):
     summary = packet.summary() if IP in packet or IPv6 in packet else "Non-IP packet"
